b902e12dc8395c946510473abc8b80e29f2229b4
Glynn Quelch <glynn.quelch@gmail.com>
2022-04-01 11:32:16 +0100

    House Keeping

--- src/Service/Parent_Page_Form_Handler.php
+++ src/Service/Parent_Page_Form_Handler.php
@@ -37,22 +37,6 @@ class Parent_Page_Form_Handler implements Hookable {
 	public const PARENT_PAGE_FORM_NONCE = 'parent_page_form_nonce';
 
 	/**
-	 * This acts as provider for page hooks
-	 *
-	 * We cant use get_plugin_page_hook() until after the page is registered
-	 * This avoids having nested hook calls.
-	 *
-	 * @param string $page
-	 * @param string|null $parent
-	 * @return string
-	 */
-	private function page_hook_provider( string $page, ?string $parent = null ): string {
-		return null === $parent
-			? "toplevel_page_{$page}"
-			: 'admin_menu_' . $parent;
-	}
-
-	/**
 	 * Allows the wiring of hook calls for this handler
 	 *
 	 * This method is required as per the Hookable interface.
@@ -64,12 +48,12 @@ class Parent_Page_Form_Handler implements Hookable {
 	public function register( Hook_Loader $loader ): void {
 
 		// Register the primary page, pre render hook.
-		$loader->admin_action( $this->page_hook_provider( Parent_Page::PAGE_SLUG ), array( $this, 'primary_page_pre_render' ) );
+		$loader->admin_action( 'toplevel_page_' . Parent_Page::PAGE_SLUG, array( $this, 'primary_page_pre_render' ) );
 
 	}
 
 	/**
-	 * Callback for the hook used before the page is renderd.
+	 * Callback for the hook used before the page is rendered.
 	 *
 	 * This is used for Form Handling
 	 *
