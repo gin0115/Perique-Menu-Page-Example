4e41e5884f782848b92ad720df140871a29413bb
Glynn Quelch <glynn.quelch@gmail.com>
2022-04-05 01:54:57 +0100

    House Keeping

--- views/public-api-row.php
+++ views/public-api-row.php
@@ -15,35 +15,35 @@
  *       Link:string,
  *       Category:string,
  *    }
- * } 
+ * }
  */
 ?>
 <div id="api-<?php echo \absint( $key ); ?>" class="api-item__container">
-		<div class="api-item__details">
-			<h3><?php echo \esc_html( $row['API'] ); ?><span>(<?php echo \esc_html( $row['Category'] ); ?>)</span>
-			</h3>
-			<p><?php echo \esc_html( $row['Description'] ); ?></p>
-			<p><?php echo \esc_url( $row['Link'] ); ?></p>
-		</div>
-		<div class="api-item__features">
-			<?php
-				printf(
-					'<p class="api-item__feature cors"><span class="dashicons dashicons-rest-api"></span> Cors : %s </p>',
-					\esc_html( ucfirst( $row['Cors'] ) ),
-				);
-			?>
-			<?php
-				printf(
-					'<p class="api-item__feature https"><span class="dashicons dashicons-%s"></span> %s </p>',
-					true === $row['HTTPS'] ? 'lock' : 'unlock',
-					true === $row['HTTPS'] ? 'HTTPS' : 'HTTP',
-				);
-			?>
-			<?php
-				printf(
-					'<p class="api-item__feature auth"><span class="dashicons dashicons-vault"></span> Auth : %s</p>',
-					'' === $row['Auth'] ? 'None' : esc_html( ucfirst( $row['Auth'] ) ),
-				);
-			?>
-		</div>
+	<div class="api-item__details">
+		<h3><?php echo \esc_html( $row['API'] ); ?><span>(<?php echo \esc_html( $row['Category'] ); ?>)</span>
+		</h3>
+		<p><?php echo \esc_html( $row['Description'] ); ?></p>
+		<p><?php echo \esc_url( $row['Link'] ); ?></p>
 	</div>
+	<div class="api-item__features">
+		<?php
+			printf(
+				'<p class="api-item__feature cors"><span class="dashicons dashicons-rest-api"></span> Cors : %s </p>',
+				\esc_html( ucfirst( $row['Cors'] ) ),
+			);
+		?>
+		<?php
+			printf(
+				'<p class="api-item__feature https"><span class="dashicons dashicons-%s"></span> %s </p>',
+				true === $row['HTTPS'] ? 'lock' : 'unlock',
+				true === $row['HTTPS'] ? 'HTTPS' : 'HTTP',
+			);
+		?>
+		<?php
+			printf(
+				'<p class="api-item__feature auth"><span class="dashicons dashicons-vault"></span> Auth : %s</p>',
+				'' === $row['Auth'] ? 'None' : esc_html( ucfirst( $row['Auth'] ) ),
+			);
+		?>
+	</div>
+</div>
